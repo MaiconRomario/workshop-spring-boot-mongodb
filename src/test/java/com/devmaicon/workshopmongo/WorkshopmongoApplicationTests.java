0236a91965b50ee9d3d8378a6a10b6cf69097621
MaiconRomario <maiconromario389@hotmail.com>
2023-03-18 16:18:16 -0300

    Project created
    	Curso MongoDB e Spring-boot

---
+++ src/test/java/com/devmaicon/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmaicon.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
