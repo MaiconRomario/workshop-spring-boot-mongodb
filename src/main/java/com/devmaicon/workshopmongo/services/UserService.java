56bff17afefea293150bb60e83d68a655d727f9a
MaiconRomario <maiconromario389@hotmail.com>
2023-03-20 18:01:17 -0300

    connection to mongodb with repository and service.

---
+++ src/main/java/com/devmaicon/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.devmaicon.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devmaicon.workshopmongo.domain.User;
+import com.devmaicon.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+}
