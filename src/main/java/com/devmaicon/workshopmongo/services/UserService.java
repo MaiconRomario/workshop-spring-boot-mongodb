108df0b5312b97c26a996eb6d0119686d49a971c
MaiconRomario <maiconromario389@hotmail.com>
2023-03-21 19:12:11 -0300

    Insert user with Post

--- src/main/java/com/devmaicon/workshopmongo/services/UserService.java
+++ src/main/java/com/devmaicon/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devmaicon.workshopmongo.domain.User;
+import com.devmaicon.workshopmongo.dto.UserDTO;
 import com.devmaicon.workshopmongo.repository.UserRepository;
 import com.devmaicon.workshopmongo.services.exception.ObjectNotFoudExeception;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 			return user.orElseThrow(() -> new ObjectNotFoudExeception("object not found"));
+	}
+	
+	public User insert(User obj) {
+			return repository.insert(obj);
+	}
+	
+	public User fromDto(UserDTO objdto) {
+		return new User(objdto.getId(), objdto.getName(), objdto.getEmail());
 	}
 }
