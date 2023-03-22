d0125c5a824b61961929857d725464b99e4d62a7
MaiconRomario <maiconromario389@hotmail.com>
2023-03-22 18:04:52 -0300

    Endpoint for get of posts of users.

--- src/main/java/com/devmaicon/workshopmongo/resources/UserResource.java
+++ src/main/java/com/devmaicon/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.devmaicon.workshopmongo.domain.Post;
 import com.devmaicon.workshopmongo.domain.User;
 import com.devmaicon.workshopmongo.dto.UserDTO;
 import com.devmaicon.workshopmongo.services.UserService;
@@ -65,6 +66,13 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
     }
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPost(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	 
 }
 
 
