2dd74e05ebf3194cab54c79f632b45c37a2e5c26
MaiconRomario <maiconromario389@hotmail.com>
2023-03-29 17:50:55 -0300

    Getting post by id

---
+++ src/main/java/com/devmaicon/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.devmaicon.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devmaicon.workshopmongo.domain.Post;
+import com.devmaicon.workshopmongo.repository.PostRepository;
+import com.devmaicon.workshopmongo.services.exception.ObjectNotFoudExeception;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> user = repository.findById(id);
+			return user.orElseThrow(() -> new ObjectNotFoudExeception("object not found"));
+	}
+}
+	
+	
